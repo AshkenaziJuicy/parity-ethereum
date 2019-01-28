ff68e52040a753df14ff4131529d0198dd45ce6e
Axel Chalon <xaxel@protonmail.com>
2019-01-28 18:28:22 +0100

    Light client RPC: implement parity_versionInfo

--- rpc/src/v1/impls/light/parity.rs
+++ rpc/src/v1/impls/light/parity.rs
@@ -24,6 +24,7 @@ use crypto::DEFAULT_MAC;
 use ethkey::{crypto::ecies, Brain, Generator};
 use ethstore::random_phrase;
 use sync::LightSyncProvider;
+use updater::VersionInfo as UpdaterVersionInfo;
 use ethcore::account_provider::AccountProvider;
 use ethcore_logger::RotatingLogger;
 
@@ -338,7 +339,7 @@ impl Parity for ParityClient {
 	}
 
 	fn version_info(&self) -> Result<VersionInfo> {
-		Err(errors::light_unimplemented(None))
+		Ok(UpdaterVersionInfo::this().into())
 	}
 
 	fn releases_info(&self) -> Result<Option<OperationsInfo>> {
